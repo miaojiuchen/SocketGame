4af7eff7520688ab0bfb5ef96ed6cff82ad86c6d
缪玖辰 <miaojiuchen@outlook.com>
2016-08-15 11:06:11 +0800

    rename objectAssign

--- src/server/index.js
+++ src/server/index.js
@@ -6,8 +6,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
-var objectAssign = require('object-assign');
-var _ = objectAssign({}, require('./utils.js'), require('underscore'));
+var oa = require('object-assign');
+var _ = oa({}, require('./utils.js'), require('underscore'));
 
 var models = require('./models.js');
 var Room = models.Room;
